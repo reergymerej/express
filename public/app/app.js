87ad275aecd089d1923ca7684cb287f61c0be896
Jeremy Greer <jeremygreer@reergymerej.com>
2014-03-29 19:21:44 -0400

    basic UI app

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,17 @@
 Ext.application({
     name: 'MEEN',
+
+    views: ['MEEN.view.Foo'],
     launch: function () {
+
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: [
+                {
+                    xtype: 'foo',
+                    html: 'a foo'
+                },
+
                 {
                     title: 'MEEN Stack',
                     html : 'Ext JS instead of Angular JS'
---
+++ public/app/view/Foo.js
@@ -0,0 +1,5 @@
+Ext.define('MEEN.view.Foo', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.foo',
+    title: 'A Foo'
+});
